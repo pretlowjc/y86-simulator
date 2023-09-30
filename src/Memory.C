bde96999f3598978841a70a09a549bdc756efdbb
pretlowjc <justin.pretlow13@gmail.com>
2023-09-30 14:42:24 -0400

    #10 Did my half of the Memory.C file but have not tested.

--- src/Memory.C
+++ src/Memory.C
@@ -2,16 +2,20 @@
 #include <iomanip>
 #include "Memory.h"
 
-//memInstance will be initialized to the single instance
-//of the Memory class
-Memory * Memory::memInstance = NULL;
+// memInstance will be initialized to the single instance
+// of the Memory class
+Memory *Memory::memInstance = NULL;
 
-/** 
+/**
  * Memory constructor
  * initializes the mem array to 0
  */
 Memory::Memory()
 {
+   for (int i = 0; i < Memory::size; i++)
+   {
+      mem[i] = 0;
+   }
 }
 
 /**
@@ -21,25 +25,33 @@ Memory::Memory()
  *
  * @return memInstance
  */
-Memory * Memory::getInstance()
+Memory *Memory::getInstance()
 {
-   return NULL;
+   if (memInstance == NULL)
+   {
+      memInstance = new Memory();
+      return memInstance;
+   }
+   else
+   {
+      return memInstance;
+   }
 }
 
 /**
  * getLong
  * returns the 64-bit word at the indicated address; sets imem_error
  * to false if the access is aligned and the address is within range;
- * otherwise sets imem_error to true. 
+ * otherwise sets imem_error to true.
  *
  * @param address of 64-bit word; access must be aligned (address % 8 == 0)
  * @return imem_error is set to true or false
  * @return returns 64-bit word at the specified address or 0 if the
  *         access is not aligned or out of range
  */
-uint64_t Memory::getLong(int32_t address, bool & imem_error)
+uint64_t Memory::getLong(int32_t address, bool &imem_error)
 {
-   //Use Memory::size to check if an address is valid
+   // Use Memory::size to check if an address is valid
    return 0;
 }
 
@@ -53,10 +65,19 @@ uint64_t Memory::getLong(int32_t address, bool & imem_error)
  * @return imem_error is set to true or false
  * @return byte at specified address or 0 if the address is out of range
  */
-uint8_t Memory::getByte(int32_t address, bool & imem_error)
+uint8_t Memory::getByte(int32_t address, bool &imem_error)
 {
-   //Use Memory::size to check if an address is valid
-   return 0;
+   // Use Memory::size to check if an address is valid
+   if (address >= 0 && address < Memory::size)
+   {
+      mem[address];
+      imem_error = false;
+   }
+   else
+   {
+      imem_error = true;
+      return 0;
+   }
 }
 
 /**
@@ -69,16 +90,16 @@ uint8_t Memory::getByte(int32_t address, bool & imem_error)
  * @param address of 64-bit word; access must be aligned (address % 8 == 0)
  * @return imem_error is set to true or false
  */
-void Memory::putLong(uint64_t value, int32_t address, bool & imem_error)
+void Memory::putLong(uint64_t value, int32_t address, bool &imem_error)
 {
-   //Use Memory::size to check if an address is valid
+   // Use Memory::size to check if an address is valid
    return;
 }
 
 /**
  * putByte
  * sets the byte (8-bits) in memory at the indicated address to the value
- * provided if the address is within range and sets imem_error to false; 
+ * provided if the address is within range and sets imem_error to false;
  * otherwise sets imem_error to true
  *
  * @param 8-bit value to be stored in memory (mem array)
@@ -86,10 +107,18 @@ void Memory::putLong(uint64_t value, int32_t address, bool & imem_error)
  * @return imem_error is set to true or false
  */
 
-void Memory::putByte(uint8_t value, int32_t address, bool & imem_error)
+void Memory::putByte(uint8_t value, int32_t address, bool &imem_error)
 {
-   //Use Memory::size to check if an address is valid
-   return;
+   // Use Memory::size to check if an address is valid
+   if (address >= 0 && address < Memory::size)
+   {
+      mem[address];
+      imem_error = false;
+   }
+   else
+   {
+      imem_error = true;
+   }
 }
 
 /**
@@ -107,31 +136,35 @@ void Memory::dump()
    bool star = false;
    bool mem_error;
 
-   //32 bytes per line (four 8-byte words)
-   for (i = 0; i < Memory::size; i+=32)
+   // 32 bytes per line (four 8-byte words)
+   for (i = 0; i < Memory::size; i += 32)
    {
-      //get the values for the current line
-      for (int32_t j = 0; j < 4; j++) currLine[j] = getLong(i+j*8, mem_error);
+      // get the values for the current line
+      for (int32_t j = 0; j < 4; j++)
+         currLine[j] = getLong(i + j * 8, mem_error);
 
-      //if they are the same as the values in the previous line then
-      //don't display them, but always display the first line
-      if (i == 0 || currLine[0] != prevLine[0] || currLine[1] != prevLine[1] 
-          || currLine[2] != prevLine[2] || currLine[3] != prevLine[3])
+      // if they are the same as the values in the previous line then
+      // don't display them, but always display the first line
+      if (i == 0 || currLine[0] != prevLine[0] || currLine[1] != prevLine[1] || currLine[2] != prevLine[2] || currLine[3] != prevLine[3])
       {
-         std::cout << std::endl << std::setw(3) << std::setfill('0') 
-                   << std::hex << i << ": "; 
-         for (int32_t j = 0; j < 4; j++) 
-             std::cout << std::setw(16) << std::setfill('0') 
-                       << std::hex << currLine[j] << " ";
+         std::cout << std::endl
+                   << std::setw(3) << std::setfill('0')
+                   << std::hex << i << ": ";
+         for (int32_t j = 0; j < 4; j++)
+            std::cout << std::setw(16) << std::setfill('0')
+                      << std::hex << currLine[j] << " ";
          star = false;
-      } else
+      }
+      else
       {
-         //if this line is exactly like the previous line then
-         //just print a * if one hasn't been printed already
-         if (star == false) std::cout << "*";
+         // if this line is exactly like the previous line then
+         // just print a * if one hasn't been printed already
+         if (star == false)
+            std::cout << "*";
          star = true;
       }
-      for (int32_t j = 0; j < 4; j++) prevLine[j] = currLine[j];
+      for (int32_t j = 0; j < 4; j++)
+         prevLine[j] = currLine[j];
    }
    std::cout << std::endl;
 }
