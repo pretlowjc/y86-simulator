4154e2ac0f09ba1fa146b43238d30c780c14888e
Michael Crespo <crespoma@appstate.edu>
2023-10-22 20:55:37 -0400

    started loader class

--- src/Loader.C
+++ src/Loader.C
@@ -69,18 +69,46 @@ bool Loader::openFile()
    //TODO
 
    //If the user didn't supply a command line argument (inputFile is NULL)
+   if (inputFile == NULL){
    //then print the Loader::usage error message and return false
+	std::cout << Loader::usage << std::endl;
+	return false;
    //(Note: Loader::usage is a static const defined in Loader.h)
+
+   }
+
+
+	String filename = inputFile->get_stdstr();
+	bool error = false;
+
 
    //If the filename is badly formed (needs to be at least 4 characters
    //long and end with .yo) then print the Loader::badfile error message 
    //and return false
-   
-   //Open the file using an std::ifstream open
+	if( filename.get_length() < 4 || filename.isSubString(".yo", 0, error)){
+		std::cout << Loader::badfile << std::endl;
+		return false;
+
+	}
+
+	   //Open the file using an std::ifstream open
    //If the file can't be opened then print the Loader::openerr message 
    //and return false
 
-   return true;  //file name is good and file open succeeded
+	else {
+		std::ifstream file;
+		file.open(inputFile->get_stdstr());
+		if(!file.is_open()) {
+			std::cout << Loader::openerr << std::endl;
+			return false;
+		}
+		return true;
+	}
+
+   
+
+
+   //file name is good and file open succeeded
 }
 
 /*
@@ -97,7 +125,7 @@ bool Loader::openFile()
 bool Loader::load()
 {
    if (!openFile()) return false;
-
+   bool error = false;
    std::string line;
    int lineNumber = 1;  //needed if an error is found
    while (getline(inf, line))
@@ -113,16 +141,69 @@ bool Loader::load()
       //
       //If the line is a data record with errors
       //then print the Loader::baddata error message and return false
-      //
+	  	int32_t addressBegin = Loader::addrbegin;
+    	int32_t addressEnd = Loader::addrend;
+        int32_t dataBegin = Loader::databegin;
+        int32_t commentIndex = Loader::comment;
+
+		bool hasError = false;
+        uint64_t data = 0;
+
+		uint32_t address = inputLine.convert2Hex(addressBegin, 4, hasError);
+		if (!inputLine.isHex(dataBegin, 8, hasError))
+         {
+            std::cout << Loader::baddata << std::endl;
+            return false; // Error in data record
+         }
+		 data = inputLine.convert2Hex(dataBegin, 8, hasError);
+
+		 if (hasError)
+         {
+            std::cout << Loader::baddata << std::endl;
+            return false; // Error in data record
+         }
+
+		 mem->putLong(data, address, hasError);
+
+		 if (hasError)
+         {
+            std::cout << Loader::baddata << std::endl;
+            return false; // Error in data record
+         }
+
+	
+    
+   
+
+
+		
+	
       //If the line is a comment record with errors
       //then print the Loader::badcomment error message and return false
       //
+	  else 
+		{
+		const char* expectedCommentFormat = "| comment";
+		if (inputLine.isSubString(expectedCommentFormat, 0, error)) {
+		// std::string commentText = inputLine.get_stdstr().substr((expectedCommentFormat)); // need help on this
+        	std::cout << "hello" << std::endl;
+		}
+
+		else {
+			std::cout<<Loader::badcomment << std::endl;
+			return false;
+		}
+
+
+		
       //Otherwise, load any data on the line into
       //memory
+
       //
       //Don't do all of this work in this method!
       //Break the work up into multiple single purpose methods
-
+		}
+		// mem -> putLong(data,address,error);
       //increment the line number for next iteration
       lineNumber++;
    }
@@ -133,4 +214,5 @@ bool Loader::load()
 //Add helper methods definitions here and the declarations to Loader.h
 //In your code, be sure to use the static const variables defined in 
 //Loader.h to grab specific fields from the input line.
+
 
