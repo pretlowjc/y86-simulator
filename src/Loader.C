b185dda24d6ed78b3234d166d2de74ed8a0197e9
pretlowjc <justin.pretlow13@gmail.com>
2023-10-25 16:39:59 -0400

    Working on Loader, coming to office hours to make up time not in lab.

--- src/Loader.C
+++ src/Loader.C
@@ -5,17 +5,18 @@
 #include "String.h"
 #include "Loader.h"
 
-/* 
+/*
  * Loader
  * Initializes the private data members
  */
-Loader::Loader(int argc, char * argv[], Memory * mem)
+Loader::Loader(int argc, char *argv[], Memory *mem)
 {
-   //this method is COMPLETE
-   this->lastAddress = -1;   //keep track of last mem byte written to for error checking
-   this->mem = mem;          //memory instance
-   this->inputFile = NULL;   
-   if (argc > 1) inputFile = new String(argv[1]);  //input file name
+   // this method is COMPLETE
+   this->lastAddress = -1; // keep track of last mem byte written to for error checking
+   this->mem = mem;        // memory instance
+   this->inputFile = NULL;
+   if (argc > 1)
+      inputFile = new String(argv[1]); // input file name
 }
 
 /*
@@ -27,30 +28,31 @@ Loader::Loader(int argc, char * argv[], Memory * mem)
  * lineNumber - number of line in input file on which error occurred (if applicable)
  * line - line on which error occurred (if applicable)
  */
-bool Loader::printErrMsg(int32_t which, int32_t lineNumber, String * line)
+bool Loader::printErrMsg(int32_t which, int32_t lineNumber, String *line)
 {
-   //this method is COMPLETE
-   static char * errMsg[Loader::numerrs] = 
-   {
-      (char *) "Usage: yess <file.yo>\n",                       //Loader::usage
-      (char *) "Input file name must end with .yo extension\n", //Loader::badfile
-      (char *) "File open failed\n",                            //Loader::openerr
-      (char *) "Badly formed data record\n",                    //Loader::baddata
-      (char *) "Badly formed comment record\n",                 //Loader::badcomment
-   };   
+   // this method is COMPLETE
+   static char *errMsg[Loader::numerrs] =
+       {
+           (char *)"Usage: yess <file.yo>\n",                       // Loader::usage
+           (char *)"Input file name must end with .yo extension\n", // Loader::badfile
+           (char *)"File open failed\n",                            // Loader::openerr
+           (char *)"Badly formed data record\n",                    // Loader::baddata
+           (char *)"Badly formed comment record\n",                 // Loader::badcomment
+       };
    if (which >= Loader::numerrs)
    {
       std::cout << "Unknown error: " << which << "\n";
-   } else
+   }
+   else
    {
-      std::cout << errMsg[which]; 
+      std::cout << errMsg[which];
       if (lineNumber != -1 && line != NULL)
       {
          std::cout << "Error on line " << std::dec << lineNumber
                    << ": " << line->get_stdstr() << std::endl;
       }
-   } 
-   return false; //load fails
+   }
+   return false; // load fails
 }
 
 /*
@@ -66,93 +68,109 @@ bool Loader::printErrMsg(int32_t which, int32_t lineNumber, String * line)
  */
 bool Loader::openFile()
 {
-   //TODO
+   // TODO
 
-   //If the user didn't supply a command line argument (inputFile is NULL)
-   //then print the Loader::usage error message and return false
-   //(Note: Loader::usage is a static const defined in Loader.h)
-   if (inputFile == NULL) 
-   {
-      // print the Loader::usage error message.
-      return false;
-   }
-
-   //If the filename is badly formed (needs to be at least 4 characters
-   //long and end with .yo) then print the Loader::badfile error message 
-   //and return false
+   // If the filename is badly formed (needs to be at least 4 characters
+   // long and end with .yo) then print the Loader::badfile error message
+   // and return false
    int32_t filenameLength = inputFile->get_length();
    bool error;
-   if ((filenameLength < 4) && (inputFile->isSubString(".yo", 0, error) == false))
+
+   // If the user didn't supply a command line argument (inputFile is NULL)
+   // then print the Loader::usage error message and return false
+   //(Note: Loader::usage is a static const defined in Loader.h)
+   if (inputFile == NULL)
    {
-      // print the Loader::badfile error message.
-      // std::cout << printErrMsg(?, ?, ?) << std::endl;
+      /*
+      print the Loader::usage error message.
+
+      params for printErrMsg(int32_t which, int32_t lineNumber, String *line)
+      which:  Loader::usage
+      lineNumber: If lineNumber is not -1 then it also prints what line the error occured.
+      *line: inputFile?
+
+      */
+      printErrMsg(Loader::usage, 1, inputFile);
       return false;
    }
-   
-   //Open the file using an std::ifstream open
-   //If the file can't be opened then print the Loader::openerr message 
-   //and return false
+   else if ((filenameLength < 4) && (inputFile->isSubString(".yo", 0, error) == false))
+   {
+      /*
+      print the Loader::badfile error message.
 
-   // std::ifstream ??;
-   /*
-   if (no open sesame)
+      params for printErrMsg(int32_t which, int32_t lineNumber, String *line)
+      which:  Loader::badfile
+      lineNumber: If lineNumber is not -1 then it also prints what line the error occured.
+      *line: inputFile?
+      */
+      printErrMsg(Loader::badfile, -1, inputFile);
+      return false;
+   }
+
+   // Open the file using an std::ifstream open
+   // If the file can't be opened then print the Loader::openerr message
+   // and return false
+
+   std::ifstream file;
+   file.open(inputFile->get_stdstr());
+
+   if (!file.is_open())
    {
       // print the Loader::openerr message.
+      printErrMsg(Loader::openerr, -1, inputFile);
       return false;
    }
-   */
-  
-   return true;  //file name is good and file open succeeded
+
+   return true; // file name is good and file open succeeded
 }
 
 /*
- * load 
+ * load
  * Opens the .yo file by calling openFile.
  * Reads the lines in the file line by line and
  * loads the data bytes in data records into the Memory.
  * If a line has an error in it, then NONE of the line will be
  * loaded into memory and the load function will return false.
  *
- * Returns true if load succeeded (no errors in the input) 
+ * Returns true if load succeeded (no errors in the input)
  * and false otherwise.
-*/   
+ */
 bool Loader::load()
 {
-   if (!openFile()) return false;
+   if (!openFile())
+      return false;
 
    std::string line;
-   int lineNumber = 1;  //needed if an error is found
+   int lineNumber = 1; // needed if an error is found
    while (getline(inf, line))
    {
-      //create a String to contain the std::string
-      //Now, all accesses to the input line MUST be via your
-      //String class methods
+      // create a String to contain the std::string
+      // Now, all accesses to the input line MUST be via your
+      // String class methods
       String inputLine(line);
-      //TODO
+      // TODO
 
-      //Note: there are two kinds of records: data and comment
-      //      A data record begins with a "0x"
+      // Note: there are two kinds of records: data and comment
+      //       A data record begins with a "0x"
       //
-      //If the line is a data record with errors
-      //then print the Loader::baddata error message and return false
+      // If the line is a data record with errors
+      // then print the Loader::baddata error message and return false
       //
-      //If the line is a comment record with errors
-      //then print the Loader::badcomment error message and return false
+      // If the line is a comment record with errors
+      // then print the Loader::badcomment error message and return false
       //
-      //Otherwise, load any data on the line into
-      //memory
+      // Otherwise, load any data on the line into
+      // memory
       //
-      //Don't do all of this work in this method!
-      //Break the work up into multiple single purpose methods
+      // Don't do all of this work in this method!
+      // Break the work up into multiple single purpose methods
 
-      //increment the line number for next iteration
+      // increment the line number for next iteration
       lineNumber++;
    }
-   return true;  //load succeeded
-
+   return true; // load succeeded
 }
 
-//Add helper methods definitions here and the declarations to Loader.h
-//In your code, be sure to use the static const variables defined in 
-//Loader.h to grab specific fields from the input line.
-
+// Add helper methods definitions here and the declarations to Loader.h
+// In your code, be sure to use the static const variables defined in
+// Loader.h to grab specific fields from the input line.
