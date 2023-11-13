d0d60a4bda6d62b51a16ff6619359007b256d701
Michael Crespo <crespoma@appstate.edu>
2023-11-13 16:31:42 -0500

    Decode stage

--- inc/DecodeStage.h
+++ inc/DecodeStage.h
@@ -10,6 +10,12 @@ class DecodeStage: public Stage
 	  void setEInput(PipeReg * ereg, uint64_t stat, uint64_t icode, uint64_t ifun, 
                      uint64_t valC, uint64_t valA,
                      uint64_t valB , uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB);
+		uint64_t setSrcA(PipeReg *ereg, uint64_t D_icode, uint64_t d_srcA, uint64_t D_rA, uint64_t RSP);
+		uint64_t setSrcB(PipeReg *ereg, uint64_t D_icode, uint64_t d_srcB, uint64_t D_rB, uint64_t RSP);
+		uint64_t setDstE(PipeReg *ereg, uint64_t D_icode, uint64_t d_dstE, uint64_t D_rB, uint64_t RSP);
+		uint64_t setDstM(PipeReg *ereg, uint64_t D_icode, uint64_t d_dstM, uint64_t D_rA);
+		uint64_t SelFwdA(PipeReg *dreg, uint64_t d_rvalA);
+		uint64_t FwdB(PipeReg *dreg, uint64_t d_rvalB);			 
    public:
       //These are the only methods called outside of the class
       bool doClockLow(PipeRegArray * pipeRegs);
--- src/DecodeStage.C
+++ src/DecodeStage.C
@@ -3,6 +3,7 @@
 #include "D.h"
 #include "F.h"
 #include "E.h"
+#include "Instruction.h"
 
 /*
  * doClockLow
@@ -15,9 +16,10 @@
  */
 bool DecodeStage::doClockLow(PipeRegArray *pipeRegs)
 {
+	bool hasError = false;
 	PipeReg *dreg = pipeRegs->getDecodeReg();
 	PipeReg *ereg = pipeRegs->getExecuteReg();
-
+	
 	uint64_t stat = dreg->get(D_STAT);
 	uint64_t icode = dreg->get(D_ICODE);
 	uint64_t ifun = dreg->get(D_IFUN);
@@ -30,12 +32,24 @@ bool DecodeStage::doClockLow(PipeRegArray *pipeRegs)
 	uint64_t dstM = RegisterFile::RNONE;
 	uint64_t srcA = RegisterFile::RNONE;
 	uint64_t srcB = RegisterFile::RNONE;
+	uint64_t d_rvalA = 0;
+	uint64_t d_rvalB = 0;
+	
 
+	//  uint64_t d_rvalA = RegisterFile::readRegister(d_srcA, hasError); -- one option
+	//  uint64_t d_rvalB = RegisterFile::readRegister(d_srcB, hasError); 
+	
 	// During compilation, numFields is an unused variable.
 	// I don't we need it... -Justin
 	// uint64_t numFields = dreg -> get(D_NUMFIELDS);
 
 	// maybe pass numfields.
+	setSrcA(ereg, icode, d_srcA, rA, RegisterFile::rsp);
+	setSrcB(ereg, icode, d_srcB, rB, RegisterFile::rsp);
+	setDstE(ereg, icode, dstE, rB, RegisterFile::rsp);
+	setDstM(ereg, icode, dstM, rA);
+	SelFwdA(dreg, d_rvalA);
+	FwdB(dreg,d_rvalB);
 	setEInput(ereg, stat, icode, ifun, valC, valA, valB, dstE, dstM, srcA, srcB);
 
 	return false;
@@ -71,3 +85,39 @@ void DecodeStage::setEInput(PipeReg *ereg, uint64_t stat, uint64_t icode,
 	ereg->set(E_SRCA, srcA);
 	ereg->set(E_SRCB, srcB);
 }
+
+uint64_t DecodeStage::setSrcA(PipeReg *ereg, uint64_t D_icode, uint64_t d_srcA, uint64_t D_rA, uint64_t RSP) {
+	if(D_icode == Instruction::IOPQ || Instruction::IRMMOVQ || Instruction::IMRMOVQ) return d_srcA == D_rA;
+	if(D_icode == Instruction::IPOPQ || Instruction::IRET) return d_srcA == RegisterFile::rsp;
+	return RegisterFile::RNONE;
+}
+
+uint64_t DecodeStage::setSrcB(PipeReg *ereg, uint64_t D_icode, uint64_t d_srcB, uint64_t D_rB, uint64_t RSP){
+	if(D_icode == Instruction::IOPQ || Instruction::IRMMOVQ || Instruction::IMRMOVQ) return d_srcB = D_rB;
+	if(D_icode == Instruction::IPUSHQ || Instruction::IPOPQ || Instruction::ICALL || Instruction::IRET) return d_srcB = RSP;
+	return RegisterFile::RNONE;
+
+}
+
+uint64_t DecodeStage::setDstE(PipeReg *ereg, uint64_t D_icode, uint64_t d_dstE, uint64_t D_rB, uint64_t RSP){
+	if(D_icode == Instruction::IRMMOVQ || Instruction::IIRMOVQ || Instruction::IOPQ) return d_dstE = D_rB;
+	if(D_icode == Instruction::IPUSHQ || Instruction:: IPOPQ || Instruction::ICALL || Instruction::IRET) return d_dstE = RSP;
+	return RegisterFile::RNONE;
+}
+
+uint64_t DecodeStage::setDstM(PipeReg *ereg, uint64_t D_icode, uint64_t d_dstM, uint64_t D_rA){
+	if(D_icode == Instruction::IMRMOVQ || Instruction::IPOPQ) return d_dstM = D_rA;
+	return RegisterFile::RNONE;
+}
+
+uint64_t DecodeStage::SelFwdA(PipeReg *dreg, uint64_t d_rvalA){
+	bool hasError = false;
+	return d_rvalA = rf->readRegister(d_srcA, hasError);//-- current option.
+	
+}
+
+uint64_t DecodeStage::FwdB(PipeReg *dreg, uint64_t d_rvalB){
+	bool hasError = false;
+	return d_rvalB = rf -> readRegister(d_srcB, hasError);
+}
+
