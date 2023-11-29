91f9202583015bb07506d2a6ca113e9f9b909fb1
pretlowjc <justin.pretlow13@gmail.com>
2023-11-29 15:20:53 -0500

    Passing 39 out of 44 tests, will come back to later. I have emailed for help. predPC and valP in cycle 2 are not correct.

--- inc/MemoryStage.h
+++ inc/MemoryStage.h
@@ -3,14 +3,18 @@
 
 #ifndef MEMORYSTAGE_H
 #define MEMORYSTAGE_H
-class MemoryStage: public Stage
+class MemoryStage : public Stage
 {
-   private:
-      //TODO: provide declarations for new methods
-	  void setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM, uint64_t dstE, uint32_t dstM);
-   public:
-      //These are the only methods called outside of the class
-      bool doClockLow(PipeRegArray * pipeRegs);
-      void doClockHigh(PipeRegArray * pipeRegs);
+private:
+   // TODO: provide declarations for new methods
+   void setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM, uint64_t dstE, uint32_t dstM);
+   uint64_t mem_addr(uint64_t M_icode, uint64_t M_valE, uint64_t M_valA);
+   bool mem_read(uint64_t M_icode);
+   bool mem_write(uint64_t M_icode);
+
+public:
+   // These are the only methods called outside of the class
+   bool doClockLow(PipeRegArray *pipeRegs);
+   void doClockHigh(PipeRegArray *pipeRegs);
 };
 #endif
--- src/MemoryStage.C
+++ src/MemoryStage.C
@@ -4,6 +4,7 @@
 #include "M.h"
 #include "E.h"
 #include "Stage.h"
+#include "Instruction.h"
 
 /*
  * doClockLow
@@ -17,15 +18,32 @@
 bool MemoryStage::doClockLow(PipeRegArray *pipeRegs)
 {
 	// Some of this is possibly wrong. - Justin
-	
-	PipeReg *mreg = pipeRegs -> getMemoryReg();
-	PipeReg *wreg = pipeRegs -> getWritebackReg();
+
+	PipeReg *mreg = pipeRegs->getMemoryReg();
+	PipeReg *wreg = pipeRegs->getWritebackReg();
+	Memory *memory = Memory::getInstance();
 	uint64_t stat = mreg->get(M_STAT);
 	uint64_t icode = mreg->get(M_ICODE);
 	uint32_t dstM = mreg->get(M_DSTM);
 	uint64_t dstE = mreg->get(M_DSTE);
-	uint64_t valE = mreg -> get(M_VALE);
+	uint64_t valE = mreg->get(M_VALE);
+	uint64_t valA = mreg->get(M_VALA);
 	m_valM = 0;
+
+	// call mem_addr method to obtain the address.
+	uint64_t address = mem_addr(icode, valE, valA);
+	// if mem_read returns true, use Memory class to read a long.
+	bool error = false;
+	if (mem_read(icode))
+	{
+		m_valM = memory->getLong(address, error);
+	}
+	// If mem_write returns true, use Memory class to write M_valA to memory.
+	if (mem_write(icode))
+	{
+		memory->putLong(valA, address, error);
+	}
+
 	setWInput(wreg, stat, icode, valE, m_valM, dstE, dstM);
 
 	return false;
@@ -40,8 +58,8 @@ bool MemoryStage::doClockLow(PipeRegArray *pipeRegs)
  */
 void MemoryStage::doClockHigh(PipeRegArray *pipeRegs)
 {
-	PipeReg *wreg = pipeRegs -> getWritebackReg();
-	wreg -> normal();
+	PipeReg *wreg = pipeRegs->getWritebackReg();
+	wreg->normal();
 }
 
 void MemoryStage::setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM, uint64_t dstE, uint32_t dstM)
@@ -54,4 +72,27 @@ void MemoryStage::setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64
 	wreg->set(W_VALM, valM);
 	wreg->set(W_DSTE, dstE);
 	wreg->set(W_DSTM, dstM);
+}
+
+uint64_t MemoryStage::mem_addr(uint64_t M_icode, uint64_t M_valE, uint64_t M_valA)
+{
+	if (M_icode == Instruction::IRRMOVQ || M_icode == Instruction::IPUSHQ || M_icode == Instruction::ICALL || M_icode == Instruction::IMRMOVQ)
+	{
+		return M_valE;
+	}
+	if (M_icode == Instruction::IPOPQ || M_icode == Instruction::IRET)
+	{
+		return M_valA;
+	}
+	return 0;
+}
+
+bool MemoryStage::mem_read(uint64_t M_icode)
+{
+	return (M_icode == Instruction::IMRMOVQ || M_icode == Instruction::IPOPQ || M_icode == Instruction::IRET);
+}
+
+bool MemoryStage::mem_write(uint64_t M_icode)
+{
+	return (M_icode == Instruction::IRMMOVQ || M_icode == Instruction::IPUSHQ || M_icode == Instruction::ICALL);
 }
