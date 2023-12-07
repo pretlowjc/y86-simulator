04b4d33059aadc22d925fb9c5454dff9133eb625
pretlowjc <justin.pretlow13@gmail.com>
2023-12-07 18:03:47 -0500

    forgot to do finalize memory stage, DONE.

--- src/MemoryStage.C
+++ src/MemoryStage.C
@@ -30,26 +30,20 @@ bool MemoryStage::doClockLow(PipeRegArray *pipeRegs)
 	uint64_t valA = mreg->get(M_VALA);
 	Stage::m_valM = 0;
 
-	// call mem_addr method to obtain the address.
 	uint64_t address = mem_addr(icode, valE, valA);
-	// if mem_read returns true, use Memory class to read a long.
+
 	bool error = false;
 	if (mem_read(icode))
 	{
 		m_valM = memory->getLong(address, error);
-	// checking if mem_error aka error? if so set m_stat to status::sadr
-		
 	}
-	// If mem_write returns true, use Memory class to write M_valA to memory.
 	else if (mem_write(icode))
 	{
 		memory->putLong(valA, address, error);
-		// adding it here as well due to it saying everytime we access memory.
-		
 	}
 	if (error)
-			m_stat = Status::SADR;
-	else 
+		m_stat = Status::SADR;
+	else
 		m_stat = stat;
 	setWInput(wreg, m_stat, icode, valE, m_valM, dstE, dstM);
 
@@ -69,10 +63,19 @@ void MemoryStage::doClockHigh(PipeRegArray *pipeRegs)
 	wreg->normal();
 }
 
+/**
+ * setWInput
+ * provides the input to potentially be stored in the W register during doClockHigh
+ * @param wreg - pointer to w register instance
+ * @param stat - value to be stored in the stat pipeline register within W
+ * @param icode - value to be stored in the icode pipeline register within W
+ * @param valE - value to be stored in the valE pipeline register within W
+ * @param valM - value to be stored in valM pipeline register within W
+ * @param dstE - value to be stored in dstE pipeline register within W
+ * @param dstM - value to be stored in dstM pipeline register within W
+ */
 void MemoryStage::setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM, uint64_t dstE, uint32_t dstM)
 {
-	// probably some things missing but these are my initial thoughts... -Justin
-
 	wreg->set(W_STAT, stat);
 	wreg->set(W_ICODE, icode);
 	wreg->set(W_VALE, valE);
@@ -81,6 +84,14 @@ void MemoryStage::setWInput(PipeReg *wreg, uint64_t stat, uint64_t icode, uint64
 	wreg->set(W_DSTM, dstM);
 }
 
+/**
+ * mem_addr
+ * this will calculate the address to be written to ?
+ * @param M_icode - value to check to see what value to pass depending on instruction
+ * @param M_valE - value to be returned for address
+ * @param M_valA - value to be returned for address
+ * @return - value of the address
+ */
 uint64_t MemoryStage::mem_addr(uint64_t M_icode, uint64_t M_valE, uint64_t M_valA)
 {
 	if (M_icode == Instruction::IRMMOVQ || M_icode == Instruction::IPUSHQ || M_icode == Instruction::ICALL || M_icode == Instruction::IMRMOVQ)
@@ -94,11 +105,24 @@ uint64_t MemoryStage::mem_addr(uint64_t M_icode, uint64_t M_valE, uint64_t M_val
 	return 0;
 }
 
+/**
+ * mem_read
+ * this finds whether a value from memory needs to be read.
+ * @param M_icode - value to be checked to see if you are reading from memory.
+ * @return - true or false if you need to read from memory.
+ */
 bool MemoryStage::mem_read(uint64_t M_icode)
 {
 	return (M_icode == Instruction::IMRMOVQ || M_icode == Instruction::IPOPQ || M_icode == Instruction::IRET);
 }
 
+/**
+ * mem_write
+ * this finds out whether or not you need to write to memory depending on the
+ * instruction.
+ * @param M_icode - value to be checked to see if anything needs to be written.
+ * @return - true or false, if you need to write to memory.
+ */
 bool MemoryStage::mem_write(uint64_t M_icode)
 {
 	return (M_icode == Instruction::IRMMOVQ || M_icode == Instruction::IPUSHQ || M_icode == Instruction::ICALL);
